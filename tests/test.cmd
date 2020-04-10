8607892092e6260aff6da6b882bb49e3bdfb2609
Scott <swilko35@gmail.com>
2020-04-10 20:17:07 +1000

    Removed code coverage files

--- tests/test.cmd
+++ tests/test.cmd
@@ -1,8 +1,9 @@
+cd tests
 cd Application.UnitTests
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./coverage/lcov.info
 cd ..\Domain.UnitTests
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./coverage/lcov.info
 cd ..\Infrastructure.IntegrationTests
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
-
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./coverage/lcov.info
+cd ..\..\
 
